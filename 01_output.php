ef1e36505ed0f2833336956df77bdc488fc10250
Misko Valent <mvalent@northislandcollege.ca>
2023-01-19 15:07:35 -0800

    watching video

--- 01_output.php
+++ 01_output.php
@@ -4,3 +4,12 @@
 /*
   A .php file running on a server, can output both HTML and PHP code. There are multiple functions that can be used to output data to the browser.
 */
+
+echo 'hello there';
+echo '<br>';
+
+print_r([1, 2, 3]);
+
+
+
+?>
